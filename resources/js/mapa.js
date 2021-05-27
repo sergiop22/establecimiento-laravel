4b867388bb20c1761204432b4f34212afa1242b6
sergiop22 <sergiopadillan97@gmail.com>
2021-05-27 15:46:04 -0500

    Agregue el mapa, aun no se finaliza el JS

---
+++ resources/js/mapa.js
@@ -0,0 +1,109 @@
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
+const provider = new OpenStreetMapProvider();
+
+document.addEventListener('DOMContentLoaded', () => {
+	if(document.querySelector('#mapa')){
+		const lat = 20.666332695977;
+	    const lng = -103.392177745699;
+
+	    const mapa = L.map('mapa').setView([lat, lng], 16);
+
+	    //eliminar pines previos
+	    let markers = new L.FeatureGroup().addTo(mapa);
+
+	    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	    }).addTo(mapa);
+	 
+	    let marker;
+
+	    // agregar el pin
+	    marker = new L.marker([lat, lng], {
+	    	draggable: true,
+	    	autoPan: true
+	    }).addTo(mapa);
+
+	    //agregar el pin a las capas
+	    markers.addLayer(marker);
+
+	    //geocode service
+	    const geocodeService = L.esri.Geocoding.geocodeService({
+            apikey: 'AAPK22263b54e6cd49cb86e23f2d29001bf1nvmGQNwGCJmDbXuLuWwXRCqGLtvi3H1fx2ielpPdRITrVtdeHSKevTSe9YbrdU_8' // reemplazamos con nuestra api key de https://developers.arcgis.com 
+        });
+
+        //buscador de direcciones
+        const buscador = document.querySelector('#formdireccion');
+        buscador.addEventListener('blur', buscarDireccion);
+
+	    reubicarPin(marker);
+
+	    function reubicarPin(marker) {
+	    	//detectar movimiento del marker
+	    	marker.on('moveend', function(e) {
+		    	marker = e.target;
+
+		    	const posicion = marker.getLatLng();
+
+		    	//centrar automaticamente
+		    	mapa.panTo( new L.LatLng(posicion.lat, posicion.lng) );
+
+		    	//reverse geocoding cuando el usuario reubica el pin
+		    	geocodeService.reverse().latlng(posicion, 16).run(function(error, resultado) {
+		    			//console.log(error);
+
+		    			console.log(resultado.address);
+
+		    			marker.bindPopup(resultado.address.LongLabel);
+		    			marker.openPopup();
+
+		    			//llenar los campos
+		    			llenarInputs(resultado);
+	    		}); 
+
+	    	});
+	    }
+
+	    function buscarDireccion() {
+	    	if(e.target.value.length > 10) {
+	    		provider.search({ query: e.target.value + ' Guadalajara MX' })
+	    			.then( resultado => {
+	    				if(resultado ) {
+	    					//limpiar pines previos
+	    					markers.clearLayers();
+
+	    					//reverse geocoding cuando el usuario reubica el pin
+				    		geocodeService.reverse().latlng(resultado[0].bounds[0], 16).run(function(error, resultado) {
+
+				    			//llenar inputs
+				    			llenarInputs(resultado);
+				    			//centrar mapa
+				    			mapa.setView(resultado.latlng)
+				    			//agregar pin
+				    			marker = new L.marker(resultado.latlng, {
+							    	draggable: true,
+							    	autoPan: true
+							    }).addTo(mapa);
+							    //asignar al contenedor el nuevo pin
+							    markers.addLayer(marker);
+				    			//mover pin
+				    			reubicarPin(marker);
+
+	    					});
+				    	}
+	    			})
+	    			.catch( error => {
+	    				console.log(error);
+	    			})
+	    	}
+	    }
+
+	    function llenarInputs(resultado) {
+			document.querySelector('#direccion').value = resultado.address.Address || '';
+			document.querySelector('#colonia').value = resultado.address.Neighborhood || '';
+			document.querySelector('#lat').value = resultado.latlng.lat || '';
+			document.querySelector('#lng').value = resultado.latlng.lng || '';
+    	}
+	}
+  		
+}); 
+  	
--- resources/views/establecimientos/create.blade.php
+++ resources/views/establecimientos/create.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
+@section('styles')
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    crossorigin=""/>
+
+  <link rel="stylesheet" href="https://unpkg.com/esri-leaflet-geocoder@3.0.0/dist/esri-leaflet-geocoder.css"
+    integrity="sha512-IM3Hs+feyi40yZhDH6kV8vQMg4Fh20s9OzInIIAc4nx7aMYMfo+IenRUekoYsHZqGkREUgx0VvlEsgm7nCDW9g=="
+    crossorigin="">
+@endsection
+
 @section('content')
 	<div class="container">
 		<h1 class="text-center mt-4">Registrar Establecimientos</h1>
@@ -60,12 +70,60 @@
 						<label for="formbuscador">Coloca la dirección del Establecimiento</label>
 						<input type="text" id="formbuscador" placeholder="Calle del establecimiento" class="form-control">
 
-						<p class="text-secondary mt-5 mb-3 text-center">El asistente colocará una dirección estimada, mueve el Pin hacia el lugar correcto</p>
+						<p class="text-secondary mt-5 mb-3 text-center">El asistente colocará una dirección estimada o  mueve el Pin hacia el lugar correcto</p>
 					</div>
+
+					<div class="form-group">
+						<div id="mapa" style="height: 400px;"></div>
+					</div>
+
+					<p class="informacion">Confirma que los siguientes campos son correctos</p>
+
+					<div class="form-group">
+						<label for="direccion">Dirección</label>
+
+						<input type="text" name="direccion" id="direccion" class="form-control @error('direccion') is-invalid @enderror" placeholder="Direccion" value="{{ old('direccion') }}">
+
+						@error('direccion')
+							<div class="invalid-feedback">
+								{{$message}}
+							</div>
+						@enderror
+					</div>
+
+					<div class="form-group">
+						<label for="colonia">Colonia</label>
+
+						<input type="text" name="colonia" id="colonia" class="form-control @error('colonia') is-invalid @enderror" placeholder="Colonia" value="{{ old('colonia') }}">
+
+						@error('colonia')
+							<div class="invalid-feedback">
+								{{$message}}
+							</div>
+						@enderror
+					</div>
+
+					<input type="hidden" id="lat" name="lat" value="{{ old('lat')}}">
+					<input type="hidden" id="lng" name="lng" value="{{ old('lng')}}">
+
 				</fieldset>
 
 			</form>
 		</div>
 	</div>
 
+@endsection
+
+@section('scripts')
+	 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+    crossorigin=""></script>
+
+    <script src="https://unpkg.com/esri-leaflet@3.0.2/dist/esri-leaflet.js"
+    integrity="sha512-myckXhaJsP7Q7MZva03Tfme/MSF5a6HC2xryjAM4FxPLHGqlh5VALCbywHnzs2uPoF/4G/QVXyYDDSkp5nPfig=="
+    crossorigin=""></script> 
+
+	<script src="https://unpkg.com/esri-leaflet-geocoder@3.0.0/dist/esri-leaflet-geocoder.js"
+    integrity="sha512-vZbMwAf1/rpBExyV27ey3zAEwxelsO4Nf+mfT7s6VTJPUbYmD2KSuTRXTxOFhIYqhajaBU+X5PuFK1QJ1U9Myg=="
+    crossorigin=""></script>
 @endsection
